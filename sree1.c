ba1b11ddf1c635c444a23df9669ef08883f71a30
srivathsa2005 <kotrike.srivathsa@s.amity.edu>
2025-07-28 20:25:34 +0530

    c1

---
+++ sree1.c
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HealthCare Hospital - Doctor Dashboard</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    
+
